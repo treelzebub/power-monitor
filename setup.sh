d58584f7b55715c2134c007d12e8613adbec9cd8
treelzebub <treelzebub@users.noreply.github.com>
2024-11-27 13:28:30 -0800

    fix setup script

--- setup.sh
+++ setup.sh
@@ -7,24 +7,24 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 read -p "Set the path for log file (eg. /home/me/): " LOG_PATH
-read -p "Set the path for your system\\'s battery (eg. /sys/class/power_supply/): " BATTERY_PATH
+
+echo "Available power supply paths:"
+ls -1 /sys/class/power_supply
+read -p "Set the path for your system's battery (eg. /sys/class/power_supply/): " BATTERY_PATH
 
 CONFIG_FILE="config"
+touch config
+sed -i "s|^LOG_PATH=.*|LOG_PATH=$LOG_PATH|" "$CONFIG_FILE" || echo "LOG_PATH=$LOG_PATH" >> "$CONFIG_FILE"
+sed -i "s|^BATTERY=.*|BATTERY=$BATTERY_PATH|" "$CONFIG_FILE" || echo "BATTERY_PATH=$BATTERY_PATH" >> "$CONFIG_FILE"
 
-if [ -f "$CONFIG_FILE" ]; then
-    sed -i "s|^LOG_PATH=.*|LOG_PATH=$LOG_PATH|" "$CONFIG_FILE" || echo "LOG_PATH=$LOG_PATH" >> "$CONFIG_FILE"
-    sed -i "s|^BATTERY=.*|BATTERY=$BATTERY_PATH|" "$CONFIG_FILE" || echo "BATTERY_PATH=$BATTERY_PATH" >> "$CONFIG_FILE"
-else
-    echo "LOG_PATH=$LOG_PATH" > "$CONFIG_FILE"
-    echo "BATTERY_PATH=$BATTERY_PATH" >> "$CONFIG_FILE"
-fi
-
-mv power_monitor.py /usr/local/bin/
-chmod +x /usr/local/bin/power_monitor.py
-mv power_monitor.service /etc/systemd/system/
+mkdir -p /etc/power_monitor/
+cp config /etc/power_monitor/
+chmod +x power_monitor.py
+cp power_monitor.py /usr/local/bin/
+cp power_monitor.service /etc/systemd/system/
 systemctl daemon-reload
-systemctl start battery_monitor.service
-systemctl enable battery_monitor.service
+systemctl enable power_monitor.service
+systemctl start power_monitor.service
 
 echo "Power monitor is configured and running."
 echo "Logs will be available at: $LOG_PATH/power_monitor.log"
